d8f2d400bd80558a7925e03dc5b9df6de468ece1
copelansam <copelansam@gmail.com>
2026-04-01 17:59:50 -0400

    Begin smart device base

--- backend/src/main/java/com/example/smarthome/domain/smartdevices/SmartDeviceBase.java
+++ backend/src/main/java/com/example/smarthome/domain/smartdevices/SmartDeviceBase.java
@@ -1,4 +1,29 @@
 package com.example.smarthome.domain.smartdevices;
 
+import java.util.UUID;
+
 public abstract class SmartDeviceBase {
+    private UUID uuid;
+    private String name;
+    private String location;
+    private DeviceType deviceType;
+
+    public SmartDeviceBase(UUID uuid, String name, String location, DeviceType deviceType){
+        this.uuid = uuid;
+        this.name = name;
+        this.location = location;
+        this.deviceType = deviceType;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getLocation(){
+        return this.location;
+    }
+    public DeviceType getDeviceType(){
+        return this.deviceType;
+    }
+
+
 }
