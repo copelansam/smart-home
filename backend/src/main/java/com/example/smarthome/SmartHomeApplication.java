e9e8d0906e28ce748f3a6ef3226a7d2c981aee1e
copelansam <copelansam@gmail.com>
2026-04-07 07:52:42 -0400

    Create state intializer to initialize state so hibernate can view them to convert states from strings in database to state objects

--- backend/src/main/java/com/example/smarthome/SmartHomeApplication.java
+++ backend/src/main/java/com/example/smarthome/SmartHomeApplication.java
@@ -1,13 +1,20 @@
 package com.example.smarthome;
 
+import com.example.smarthome.domain.smartdevices.statemachine.states.StateInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.smarthome")
 public class SmartHomeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartHomeApplication.class, args);
+
+        StateInitializer.init();
+
+        SpringApplication.run(SmartHomeApplication.class, args);
+
 	}
 
 }
--- backend/src/main/java/com/example/smarthome/domain/smartdevices/statemachine/states/StateInitializer.java
+++ backend/src/main/java/com/example/smarthome/domain/smartdevices/statemachine/states/StateInitializer.java
@@ -1,4 +1,38 @@
 package com.example.smarthome.domain.smartdevices.statemachine.states;
 
+import com.example.smarthome.domain.smartdevices.statemachine.states.doorlockstates.DoorLockedState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.doorlockstates.DoorUnlockedState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.fanstates.FanOffState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.fanstates.FanOnState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.lightstates.LightOffState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.lightstates.LightOnState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.thermostatstates.ThermostatCoolingState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.thermostatstates.ThermostatHeatingState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.thermostatstates.ThermostatIdleState;
+import com.example.smarthome.domain.smartdevices.statemachine.states.thermostatstates.ThermostatOffState;
+
 public class StateInitializer {
+    public static void init() {
+
+        Class<?>[] classes = {
+                DoorUnlockedState.class,
+                DoorLockedState.class,
+                FanOnState.class,
+                FanOffState.class,
+                LightOnState.class,
+                LightOffState.class,
+                ThermostatOffState.class,
+                ThermostatIdleState.class,
+                ThermostatHeatingState.class,
+                ThermostatCoolingState.class
+        };
+
+        for (Class<?> c : classes) {
+            try {
+                Class.forName(c.getName());
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
